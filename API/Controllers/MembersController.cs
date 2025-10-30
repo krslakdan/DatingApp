041c77b65b687fc6ece9e0855a23ef9ff38f6c23
krslakdan <191279454+krslakdan@users.noreply.github.com>
2025-10-30 23:05:21 +0100

    Implement base controller to avoid repeating

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,14 +1,11 @@
 ﻿using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MembersController(AppDbContext db) : ControllerBase
+    public class MembersController(AppDbContext db) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
