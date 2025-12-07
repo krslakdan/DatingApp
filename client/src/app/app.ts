d786ef8455bbc85d82463413c757ae1409d7f47e
krslakdan <191279454+krslakdan@users.noreply.github.com>
2025-12-07 19:56:38 +0100

    Implement logic for managing current user and add home page

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,23 +2,36 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import {Nav} from '../layout/nav/nav';
+import {AccountService} from '../core/services/account-service';
+import {Home} from '../features/home/home';
 
 @Component({
   selector: 'app-root',
   imports: [
-    Nav
+    Nav,
+    Home
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit{
+  private accountService=inject(AccountService);
   private http=inject(HttpClient);
   protected readonly title = 'Dating app';
   protected members=signal<any>([]);
 
     async ngOnInit(){
     this.members.set(await this.getMembers());
+    this.setCurrentUser();
   }
+
+  setCurrentUser(){
+      const userString=localStorage.getItem('user');
+      if(!userString)return;
+      const user=JSON.parse(userString);
+      this.accountService.currentUser.set(user);
+  }
+
   async getMembers(){
       try{
         return lastValueFrom(this.http.get("https://localhost:7023/api/Members"));
